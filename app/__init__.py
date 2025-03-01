af44888960d61be027d3f23bd2e608a03f43b99b
Eric Richard <reric0802@gmail.com>
2025-03-01 11:50:54 +0300

    added a webhook route

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, jsonify
 from .extensions import db, cors  # Removed `migrate`
 
 load_dotenv()
@@ -39,6 +39,12 @@ def create_app():
     app.register_blueprint(orders_bp)
     app.register_blueprint(received_bp)
     app.register_blueprint(home_bp)
+
+    @app.route('/github-webhook/', methods=['POST'])
+    def github_webhook():
+        data = request.json
+        print("Received Webhook:", data)  # Log the webhook data
+        return jsonify({"message": "Webhook received"}), 200
 
     return app
 
